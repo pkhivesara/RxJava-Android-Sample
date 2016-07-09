f28d201588d89be65d856147877f102560387930
prats.khive <prats.khive@gmail.com>
2016-07-09 12:09:50 -0700

    switch to lambda and static reference

--- app/src/main/java/com/app/materialdesigndemo_rxjava/app/presenters/LoginFragmentPresenter.java
+++ app/src/main/java/com/app/materialdesigndemo_rxjava/app/presenters/LoginFragmentPresenter.java
@@ -1,101 +1,45 @@
 package com.app.materialdesigndemo_rxjava.app.presenters;
 
-import android.database.Observable;
 import android.graphics.Color;
-import android.graphics.Interpolator;
 import android.text.TextUtils;
 import android.widget.EditText;
-
 import com.jakewharton.rxbinding.widget.RxTextView;
-
-import rx.Subscriber;
-import rx.functions.Func1;
-import rx.functions.Func2;
-
 import static android.util.Patterns.EMAIL_ADDRESS;
 
 
 public class LoginFragmentPresenter {
-	LoginFragmentPresenterInterface loginFragmentPresenterInterface;
+    LoginFragmentPresenterInterface loginFragmentPresenterInterface;
 
-	public LoginFragmentPresenter(LoginFragmentPresenterInterface loginFragmentPresenterInterface) {
-		this.loginFragmentPresenterInterface = loginFragmentPresenterInterface;
+    public LoginFragmentPresenter(LoginFragmentPresenterInterface loginFragmentPresenterInterface) {
+        this.loginFragmentPresenterInterface = loginFragmentPresenterInterface;
 
-	}
+    }
 
-	public void setupListenersForViews(EditText usernameEditText, EditText passwordEditText) {
-		rx.Observable<Boolean> userNameObservableValid = RxTextView.textChanges(usernameEditText).
-				map(new Func1<CharSequence, String>() {
-					@Override
-					public String call(CharSequence charSequence) {
-						return charSequence.toString();
-					}
-				}).
-				map(new Func1<String, Boolean>() {
-					@Override
-					public Boolean call(String charSequence) {
-						return !TextUtils.isEmpty(charSequence) && EMAIL_ADDRESS.matcher(charSequence).matches();
-					}
-				});
-		userNameObservableValid.map(new Func1<Boolean, Integer>() {
-			@Override
-			public Integer call(Boolean aBoolean) {
-				return aBoolean ? Color.BLUE : Color.RED;
-			}
-		}).subscribe(integer1 -> loginFragmentPresenterInterface.setEmailTextColor(integer1));
+    public void setupListenersForViews(EditText usernameEditText, EditText passwordEditText) {
 
-		rx.Observable<Boolean> passwordObservableValid = RxTextView.textChanges(passwordEditText).
-				map(new Func1<CharSequence, String>() {
-					@Override
-					public String call(CharSequence charSequence) {
-						return charSequence.toString();
-					}
-				}).
-				map(new Func1<String, Boolean>() {
-					@Override
-					public Boolean call(String s) {
-						return !TextUtils.isEmpty(s) && s.length() > 4;
+        /*Set email address field color based on boolean observable*/
+        rx.Observable<Boolean> userNameObservableValid = RxTextView.textChanges(usernameEditText).
+                map(CharSequence::toString).
+                map(charSequence -> !TextUtils.isEmpty(charSequence) && EMAIL_ADDRESS.matcher(charSequence).matches());
+        userNameObservableValid.map(aBoolean -> aBoolean ? Color.BLUE : Color.RED).subscribe(integer1 -> loginFragmentPresenterInterface.setEmailTextColor(integer1));
 
-					}
-				});
+        /*Set password field color based on boolean observable*/
+        rx.Observable<Boolean> passwordObservableValid = RxTextView.textChanges(passwordEditText).
+                map(CharSequence::toString).
+                map(s -> !TextUtils.isEmpty(s) && s.length() > 4);
+        passwordObservableValid.map(aBoolean -> aBoolean ? Color.BLUE : Color.RED).subscribe(integer -> loginFragmentPresenterInterface.setPasswordTextColor(integer));
 
-		passwordObservableValid.map(new Func1<Boolean, Integer>() {
-			@Override
-			public Integer call(Boolean aBoolean) {
-				return aBoolean ? Color.BLUE : Color.RED;
-			}
-		}).subscribe(integer -> loginFragmentPresenterInterface.setPasswordTextColor(integer));
 
-		rx.Observable.combineLatest(userNameObservableValid, passwordObservableValid, new Func2<Boolean, Boolean, Boolean>() {
+         /*Set button state based on an boolean observable from email and password observables*/
+        rx.Observable.combineLatest(userNameObservableValid, passwordObservableValid, (aBoolean, aBoolean2) -> aBoolean && aBoolean2)
+                .subscribe(aBoolean1 -> loginFragmentPresenterInterface.handleButtonState(aBoolean1));
+    }
 
-			@Override
-			public Boolean call(Boolean aBoolean, Boolean aBoolean2) {
-				return aBoolean && aBoolean2;
-			}
+    public interface LoginFragmentPresenterInterface {
+        void setEmailTextColor(int color);
 
-		}).subscribe(new Subscriber<Boolean>() {
-			@Override
-			public void onCompleted() {
+        void setPasswordTextColor(int color);
 
-			}
-
-			@Override
-			public void onError(Throwable e) {
-
-			}
-
-			@Override
-			public void onNext(Boolean aBoolean) {
-				loginFragmentPresenterInterface.handleButtonState(aBoolean);
-			}
-		});
-	}
-
-	public interface LoginFragmentPresenterInterface {
-		void setEmailTextColor(int color);
-
-		void setPasswordTextColor(int color);
-
-		void handleButtonState(boolean value);
-	}
+        void handleButtonState(boolean value);
+    }
 }
